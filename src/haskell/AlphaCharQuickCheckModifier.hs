e6357673e1a711297cffe3226f4ff3d082da5ccc
Olaoluwa Mustapha <37044906+OlaoluwaM@users.noreply.github.com>
2024-08-21 21:45:48 -0500

    chore: Snippets update

--- src/haskell/AlphaCharQuickCheckModifier.hs
+++ src/haskell/AlphaCharQuickCheckModifier.hs
@@ -1,14 +1,22 @@
 {-# LANGUAGE TypeApplications #-}
 
 import Test.QuickCheck
+import Data.String (IsString (fromString))
+
+-- https://stackoverflow.com/questions/20934506/haskell-quickcheck-how-to-generate-only-printable-strings
 
 newtype AlphaChar = AlphaChar {unAlphaChar :: Char} deriving (Eq, Show)
 
 genAlphaChar :: Gen AlphaChar
 genAlphaChar = do
-    let lowerCaseChar = AlphaChar <$> arbitrary @Char `suchThat` isLowerCase
-    let upperCaseChar = AlphaChar <$> arbitrary @Char `suchThat` isUpperCase
+    let lowerCaseChar = AlphaChar <$> elements ['a' .. 'z']
+    let upperCaseChar = AlphaChar <$> elements ['A' .. 'Z']
     oneof [lowerCaseChar, upperCaseChar]
 
 instance Arbitrary AlphaChar where
     arbitrary = genAlphaChar
+
+newtype AlphabeticString a = AlphabeticString {unAlphabeticString :: a} deriving (Eq, Show, Ord)
+
+instance (IsString a) => Arbitrary (AlphabeticString a) where
+    arbitrary = AlphabeticString . fromString <$> listOf ((.unAlphaChar) <$> genAlphaChar)
